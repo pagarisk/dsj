dd22ee72ee9a9396a956a9ff128bbe99176da06b
Pavel Chernyakov <chrnkvp@gmail.com>
2016-07-11 23:52:37 +0300

    made most of the stuff working

--- dsj.py
+++ dsj.py
@@ -1,27 +1,30 @@
 import os
 import sqlite3
-from flask import Flask, render_template, request, url_for, g, flash, abort
+from flask import Flask, render_template, request, url_for, redirect, g, flash, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import Form
+from wtforms import StringField, TextAreaField, IntegerField, validators
 from datetime import datetime
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.secret_key = '123'
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.path.join(app.root_path, 'dsj.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dsj.db'
 db = SQLAlchemy(app)
 
-"""
-app.config.update(dict(
-    DATABASE = os.path.join(app.root_path, 'dsj.db'),
-    SECRET_KEY = 'development key',
-    USERNAME = 'admin',
-    PASSWORD = 'admin'
-))
-"""
-# app.config.from_envvar('DSJ_SETTINGS', silent = True)
-
 # class definitions
+class JobForm(Form):
+    title = StringField('Title', [validators.Length(min = 3, max = 50)])
+    company = StringField('Company', [validators.Length(min = 1, max = 50)])
+    location = StringField('Location', [validators.Length(min = 2, max = 100)])
+    applylink = StringField('Application link')
+    companylink = StringField('Company website')
+    summary = TextAreaField('Job summary', [validators.Length(min = 1, max = 2000)])
+    requirements = TextAreaField('Job requirements', [validators.Length(min = 1, max = 2000)])
+    about = TextAreaField('About company', [validators.Length(min = 1, max = 2000)])
+
+
 class Job(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     status = db.Column(db.String(50))
@@ -29,69 +32,27 @@ class Job(db.Model):
     title = db.Column(db.String(100))
     company = db.Column(db.String(100))
     location = db.Column(db.String(50))
-    salary = db.Column(db.Integer)
     applylink = db.Column(db.String(200))
     companylink = db.Column(db.String(200))
     summary = db.Column(db.String(2000))
     requirements = db.Column(db.String(2000))
     about = db.Column(db.String(2000))
 
-    def __init__(self, title, company, location, salary, \
+    def __init__(self, title, company, location,
             applylink, companylink, summary, requirements, about):
         self.status = 'Active'
-        self.date = datetime.now()
+        self.date = datetime.today().date()
         self.title = title
         self.company = company
         self.location = location
-        self.salary = salary
         self.applylink = applylink
         self.companylink = companylink
         self.summary = summary
         self.requirements = requirements
+        self.about = about
 
     def __repr__(self):
         return '<Job %r>' % self.id
-
-
-
-
-# DB functions
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-
-def init_db():
-    """Initializes the database."""
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Creates the database tables."""
-    init_db()
-    print('Initialized the database.')
-
-
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
 
 
 # URL routings
@@ -102,9 +63,7 @@ def homepage():
 
 @app.route('/job/<int:id>')
 def job(id):
-    db = get_db()
-    cur = db.execute('SELECT * FROM jobs WHERE id = %d' % id)
-    job = cur.fetchone()
+    job = Job.query.filter_by(id = id).first_or_404()
     if job:
         return render_template("job.html", job = job)
     else:
@@ -114,35 +73,24 @@ def job(id):
 def form():
     return render_template("add.html")
 
-@app.route('/add', methods = ['POST'])
+@app.route('/add', methods = ['GET', 'POST'])
 def add_job():
-    """
-    db = get_db()
-    db.execute('INSERT INTO jobs (status, title, summary, requirements, about, location, applylink, salary, company) values ("active", ?, ?, ?, ?, ?, ?, ?, ?)',
-            [request.form['status'],
-            request.form['title'],
-            request.form['summary'],
-            request.form['requirements'],
-            request.form['about'],
-            request.form['location'],
-            request.form['applylink'],
-            request.form['salary'],
-            request.form['company']])
-    db.commit()
-    print (request.form)
-    """
-    new_job = Job(request.form['title'], \
-            request.form['company'], \
-            request.form['location'], \
-            request.form['salary'], \
-            request.form['applylink'], \
-            request.form['companylink'], \
-            request.form['summary'], \
-            request.form['requirements'], \
-            request.form['about'], \
-            )
-    flash('Thank you! Your posting has been submitted.')
-    return redirect(url_for('home'))
+    form = JobForm(request.form)
+    if request.method == 'POST' and form.validate():
+        new_job = Job(form.title.data,
+                form.company.data,
+                form.location.data,
+                form.applylink.data,
+                form.companylink.data,
+                form.summary.data,
+                form.requirements.data,
+                form.about.data)
+        db.session.add(new_job)
+        db.session.commit()
+        flash('Thank you! Your job posting has been submitted.')
+        return redirect(url_for('homepage'))
+    return render_template("add.html", form = form)
+
 
 # run only if the file is called directly
 if __name__ == '__main__':
